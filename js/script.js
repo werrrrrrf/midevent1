dbe2a1d5f28833950ac260e11966e60a7926e0fd
werrrrrrf <150002145+werrrrrrf@users.noreply.github.com>
2023-11-18 02:03:02 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
